d048c30dd9e8d268ddbac05c706b684c7e8c867b
vak-rashu <rashu.cloud11@gmail.com>
2026-08-26 07:33:15 +0000

    basic seccomp function created

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/vak-rashu/container-runtime-scratch
+
+go 1.26.7
+
+require github.com/seccomp/libseccomp-golang v0.11.1
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"syscall"
+
+	libseccomp "github.com/seccomp/libseccomp-golang"
+)
+
+var syscalls = []string{
+	"rt_sigaction", "mkdirat", "clone", "mmap", "readlinkat", "futex", "rt_sigprocmask",
+	"mprotect", "write", "sigaltstack", "gettid", "read", "open", "close", "fstat", "munmap",
+	"brk", "access", "execve", "getrlimit", "arch_prctl", "sched_getaffinity", "set_tid_address", "set_robust_list",
+}
+
+func whitelist(syscalls []string) {
+	filter, err := libseccomp.NewFilter(libseccomp.ActErrno.SetReturnCode(int16(syscall.EPERM)))
+	if err != nil {
+		fmt.Printf("Error running %v", err)
+	}
+
+	for _, element := range syscalls {
+		fmt.Println(element)
+		syscallID, err := libseccomp.GetSyscallFromName(element)
+		if err != nil {
+			panic(err)
+		}
+		filter.AddRule(syscallID, libseccomp.ActAllow)
+	}
+
+	filter.Load()
+}
+
+func main() {
+
+	whitelist(syscalls)
+
+	err := syscall.Mkdir("/home/rashu/devfest", 0755)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Folder created!!")
+	}
+}
