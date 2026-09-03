907418f5fb9860740cc5b44528724cbe64ce6718
vak-rashu <rashu.cloud11@gmail.com>
2026-09-03 10:04:22 +0000

    add child() func

--- container.go
+++ container.go
@@ -6,8 +6,26 @@ import (
 	"syscall"
 )
 
-func main() {
+func child() {
 	cmd := exec.Command("/bin/bash")
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	syscall.Mount("/home/rashu/bundle/rootfs",
+		"/home/rashu/bundle/rootfs",
+		"",
+		syscall.MS_BIND|syscall.MS_REC,
+		"")
+
+	syscall.PivotRoot("/home/rashu/bundle/rootfs", "oldroot")
+	os.Chdir("/")
+	syscall.Mount("proc", "proc", "proc", 0, "")
+	cmd.Run()
+}
+
+func main() {
+	cmd := exec.Command("/proc/self/exe/", append([]string{"child"}, "/bin/bash")...)
 
 	// gets connected to the given file
 
@@ -15,10 +33,13 @@ func main() {
 	// from the bash shell
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
+
+	child()
 
 	// ps cant see new process if its in the older process
 	// but with a new mount space it can see the new ps
