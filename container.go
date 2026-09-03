cd41193db96773ce4df072a6c4a43de4d232224f
vak-rashu <rashu.cloud11@gmail.com>
2026-09-03 12:38:45 +0000

    reexec pattern

--- container.go
+++ container.go
@@ -1,47 +1,48 @@
 package main
 
-import (
-	"os"
-	"os/exec"
-	"syscall"
-)
+// import (
+// 	"os"
+// 	"os/exec"
+// 	"syscall"
+// )
 
-func child() {
-	cmd := exec.Command("/bin/bash")
-	cmd.Stdin = os.Stdin
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
+// func child() {
+// 	cmd := exec.Command("/bin/sh")
+// 	cmd.Stdin = os.Stdin
+// 	cmd.Stderr = os.Stderr
+// 	cmd.Stdout = os.Stdout
 
-	syscall.Mount("/home/rashu/bundle/rootfs",
-		"/home/rashu/bundle/rootfs",
-		"",
-		syscall.MS_BIND|syscall.MS_REC,
-		"")
+// 	syscall.Mount("/root/bundle/rootfs",
+// 		"/root/bundle/rootfs",
+// 		"",
+// 		syscall.MS_BIND|syscall.MS_REC,
+// 		"")
 
-	syscall.PivotRoot("/home/rashu/bundle/rootfs", "oldroot")
-	os.Chdir("/")
-	syscall.Mount("proc", "proc", "proc", 0, "")
-	cmd.Run()
-}
+// 	syscall.PivotRoot("/root/bundle/rootfs", "./oldroot")
+// 	// syscall.Chroot("/home/rashu/bundle/rootfs")
+// 	os.Chdir("/")
+// 	syscall.Mount("proc", "proc", "proc", 0, "")
+// 	cmd.Run()
+// }
 
-func main() {
-	cmd := exec.Command("/proc/self/exe/", append([]string{"child"}, "/bin/bash")...)
+// func main() {
+// 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, "/bin/bash")...)
 
-	// gets connected to the given file
+// 	// gets connected to the given file
 
-	// it requires stdin to take the arguments
-	// from the bash shell
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+// 	// it requires stdin to take the arguments
+// 	// from the bash shell
+// 	cmd.Stdin = os.Stdin
+// 	cmd.Stdout = os.Stdout
+// 	cmd.Stderr = os.Stderr
 
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
-	}
+// 	cmd.SysProcAttr = &syscall.SysProcAttr{
+// 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+// 	}
 
-	child()
+// 	child()
 
-	// ps cant see new process if its in the older process
-	// but with a new mount space it can see the new ps
-	cmd.Run()
-}
+// 	// ps cant see new process if its in the older process
+// 	// but with a new mount space it can see the new ps
+// 	cmd.Run()
+// }
--- main.go
+++ main.go
@@ -42,3 +42,80 @@ package main
 // 		fmt.Println("Folder created!!")
 // 	}
 // }
+
+// import (
+// 	"fmt"
+// 	"os"
+// 	"os/exec"
+// 	"syscall"
+// )
+
+// // go run main.go run <cmd> <args>
+// func main() {
+// 	// run()
+// 	switch os.Args[1] {
+// 	case "run":
+// 		run()
+// 	case "child":
+// 		child()
+// 	default:
+// 		panic("help")
+// 	}
+// }
+
+// func run() {
+// 	fmt.Printf("Running %v \n", os.Args[2:])
+
+// 	// reexec pattern
+// 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+
+// 	// cmd := exec.Command("/bin/bash")
+// 	fmt.Println(os.Getpid())
+// 	cmd.Stdin = os.Stdin
+// 	cmd.Stdout = os.Stdout
+// 	cmd.Stderr = os.Stderr
+// 	cmd.SysProcAttr = &syscall.SysProcAttr{
+// 		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+// 		Unshareflags: syscall.CLONE_NEWNS,
+// 	}
+
+// 	must(cmd.Run())
+// }
+
+// func child() {
+// 	fmt.Printf("Running %v \n", os.Args[2:])
+
+// 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+// 	cmd.Stdin = os.Stdin
+// 	cmd.Stdout = os.Stdout
+// 	cmd.Stderr = os.Stderr
+
+// 	// must(syscall.Sethostname([]byte("container")))
+// 	// must(syscall.Chroot("/home/liz/ubuntufs"))
+// 	// must(os.Chdir("/"))
+// 	// must(syscall.Mount("proc", "proc", "proc", 0, ""))
+// 	// must(syscall.Mount("thing", "mytemp", "tmpfs", 0, ""))
+
+// 	// cg()
+
+// 	must(cmd.Run())
+
+// 	// must(syscall.Unmount("proc", 0))
+// 	// must(syscall.Unmount("thing", 0))
+// }
+
+// // func cg() {
+// // 	cgroups := "/sys/fs/cgroup/"
+// // 	pids := filepath.Join(cgroups, "pids")
+// // 	os.MkdirAll(filepath.Join(pids, "liz"), 0755)
+// // 	must(ioutil.WriteFile(filepath.Join(pids, "liz/pids.max"), []byte("20"), 0700))
+// // 	// Removes the new cgroup in place after the container exits
+// // 	must(ioutil.WriteFile(filepath.Join(pids, "liz/notify_on_release"), []byte("1"), 0700))
+// // 	must(ioutil.WriteFile(filepath.Join(pids, "liz/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
+// // }
+
+// func must(err error) {
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
